66e90efd883dc37fc53c175148ea070cfd65a3dc
danieljardine04 <danogaryjardine@gmail.com>
2021-11-19 19:53:52 -0700

    Did the JavaScript for the first quiz page and made it functional.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 var headerEl = document.querySelector("#head");
 var answerListEl = document.querySelector("#answerList");
+var pageContentEl = document.querySelector(".page-content");
 var timerEl = document.getElementById('timer');
-var button = document.createElement("<button class='btn'></button>");
+var buttonEl = document.createElement("button");
+timerEl.textContent = "Time: " + 0;
+
 
 
 
@@ -20,6 +23,38 @@ var countdown = function(){
 
     timeInterval;
 }
+
+var quizFlow = function(){
+    startUp();
+
+}
+
+var startUp = function(event){
+    
+    headerEl.textContent = "Final Fantasy Code Quiz!";
+    var paragraphEl = document.createElement("p");
+    pageContentEl.appendChild(paragraphEl);
+    paragraphEl.textContent = "Welcome to the Final Fantasy JavaScript code quiz. Please click the start button below to start the quiz. You will have 75 seconds to complete the quiz. Keep in mind that getting a wrong answer will penalize you 10 seconds each."
+    pageContentEl.appendChild(buttonEl);
+    buttonEl.textContent = "Start Quiz!"
+    
+    
+    pageContentEl.addEventListener("click", function(){
+        paragraphEl.textContent = "";
+        headerEl.textContent = "";
+        pageContentEl.remove(buttonEl);
+        countdown();
+        
+    })
+    
+};
+
+
+quizFlow();
+
+
+    
+
 
 
 
